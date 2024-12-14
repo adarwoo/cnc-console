cb9720c44411ccbcb718b6ca10f1228f76fff043
guillaume.arreckx <software@arreckx.com>
2024-12-14 21:19:16 +0100

    Using gitman

--- asx/logger/src/logger_disabled.c
+++
@@ -1,5 +0,0 @@
-/**
- * @file
- * Empty place holder used to create an empty lib.
- * This is for builds turning the logging OFF completely
- */
--- asx/src/i2c_master.cpp
+++
@@ -1,9 +0,0 @@
-#include <asx/i2c_master.hpp>
-
-extern "C"
-{
-    // Interrupt handler
-    ISR(TWI0_TWIM_vect) {
-        asx::i2c::Master::interrupt_handler();
-    }
-}
