af3a33faf7422120a1b9e61a322ed4441bb9d2b0
Adarwoo <software@arreckx.com>
2025-01-06 15:50:10 +0100

    Added modbus

--- src/console.cpp
+++ src/console.cpp
@@ -1,28 +1,12 @@
-#include <asx/reactor.hpp>
-#include <asx/uart.hpp>
+#include <piezzo.h>
 
-#include "datagram.hpp"
+#include "console.hpp"
 #include "mux.hpp"
 
-#include <piezzo.h>
 
 using namespace asx;
 
 namespace console {
-    /** Define the Usart to use by the rs485 */
-    using UartConfig = uart::CompileTimeConfig<
-        115200,                       // Baudrate
-        asx::uart::width::_8,         // Width standard 8 bits per frame
-        asx::uart::parity::even,      // Even parity (standard for Modbus)
-        asx::uart::stop::_1,          // Unique stop bit (standard in Modbus)
-        // Force RS485 mode and one wire (Rx pin is not used)
-        asx::uart::rs485 | asx::uart::onewire
-    >;
-
-    using Uart = asx::uart::Uart<1, UartConfig>;    
-
-    using modbus_slave = asx::modbus::Slave<Datagram, Uart>;
-
     /// @brief  Read 4 bits for the switch
     void on_get_sw_status() {
         Datagram::set_size(2);
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,17 @@
 /*
- * Relay modbus device main entry point.
- * The relays are initialised by the static constructor
+ * Console modbus device main entry point.
  */
-#include <chrono>
 #include <asx/reactor.hpp>
+
+#include "console.hpp"
 #include "mux.hpp"
-
-using namespace asx;
-using namespace std::chrono;
-
-
-void seq() {
-   static uint16_t c = 1;
-
-   console::mux::set_leds(c);
-
-   if ( c == 0x40 ) {
-      c = 0x0100;
-   } else if ( c == 0x4000 ) {
-      c = 1;
-   }
-
-   c <<= 1;
-}
-
 
 
 int main()
 {
+   console::modbus_slave::init();
    console::mux::init();
-   reactor::bind(seq).repeat(500ms);
 
    // Run the reactor/scheduler
-   reactor::run();
+   asx::reactor::run();
 }
